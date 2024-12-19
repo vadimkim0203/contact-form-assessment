5638794228384f074e5530ea4142f2ee58900fa2
Vadim Kim <vkim0203@gmail.com>
2024-12-19 16:16:34 +0900

    stylings added to a button and a paragraph

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,89 @@
+
 <template>
   <h1>Связаться с нами</h1>
   <form @submit.prevent="submitForm">
     <div>
-      <input
-        type="name"
-        placeholder="Имя"
-        name="name"
-        v-model="name"
-        required
-      />
+      <input type="name" placeholder="Имя" name="name" v-model="name" required />
     </div>
     <div>
-      <input
-        type="phone"
-        placeholder="Номер телефона"
-        name="phone"
-        v-model="phone"
-        required
-      />
+      <input class="required-input" type="phone" placeholder="Номер телефона" name="phone" v-model="phone" required />
     </div>
     <div>
-      <input
-        type="email"
-        placeholder="Email"
-        name="email"
-        v-model="email"
-        required
-      />
+      <input class="required-input" type="email" placeholder="Email" name="email" v-model="email" required />
     </div>
     <div>
-      <input
-        type="text"
-        placeholder="Интересующий товар/услуга"
-        name="subject"
-        v-model="subject"
-        required
-      />
+      <input type="text" placeholder="Интересующий товар/услуга" name="subject" v-model="subject" required />
     </div>
 
     <div>
-      <textarea
-        name="message"
-        rows="6"
-        placeholder="Сообщение"
-        v-model="message"
-        required
-      ></textarea>
+      <textarea name="message" rows="6" placeholder="Сообщение" v-model="message" required></textarea>
     </div>
 
-    <p>Отправляя заявку Вы соглашаетесь с политикой конфиденциальности</p>
-
-    <button type="submit">Отправить</button>
+    <p class="privacy-policy">Отправляя заявку Вы соглашаетесь с политикой конфиденциальности</p>
+    <button class="submit-button" type="submit">Отправить</button>
+    
   </form>
 </template>
 
+<style scoped>
+
+
+.privacy-policy {
+  color: red;
+}
+.submit-button {
+  background-color: #333333;
+  width:222px;
+  height: 71px;
+  color: white;
+  padding: 15px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: center;
+}
+
+.submit-button:hover {
+  background-color: #666666;
+}
+</style>
+
 <script>
-  export default {
-    data() {
-      return {
-        name:"",
-        phone:"",
-        email: "",
-        subject: "",
-        message: "",
-      };
+export default {
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+  },
+  methods: {
+    async submitForm() {
+      const response = await fetch("https://formbold.com/s/unique_form_id", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: this.email,
+          phone: this.phone,
+          email: this.email,
+          subject: this.subject,
+          message: this.message,
+        }),
+      });
+      const result = await response.json();
+      if (response.ok) {
+        alert(result.message);
+        this.$el.reset();
+      } else {
+        alert(response.message || "Something went wrong");
+      }
     },
-    methods: {
-      async submitForm() {
-        const response = await fetch("https://formbold.com/s/unique_form_id", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            name: this.email,
-            phone: this.phone,
-            email: this.email,
-            subject: this.subject,
-            message: this.message,
-          }),
-        });
-        const result = await response.json();
-        if (response.ok) {
-          alert(result.message);
-          this.$el.reset();
-        } else {
-          alert(response.message || "Something went wrong");
-        }
-      },
-    },
-  };
+  },
+};
 </script>
