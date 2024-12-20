61a7d88623b59b8f9690dc74669006ab0247bc9f
Vadim Kim <vkim0203@gmail.com>
2024-12-20 15:20:19 +0900

    Css classes revised, containers reconsidered

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,94 @@
 <template>
-  <div class="header-form-container">
-    <h1>Связаться с нами</h1>
-    <div class="form-image-container">
-      <div class="form-container">
-        <form @submit.prevent="submitForm">
-          <div>
-            <input type="name" placeholder="Имя" name="name" v-model="name" required />
-          </div>
-          <div class="input-wrapper">
-            <div class="required-input-wrapper">
-            <input class="required-input" type="phone" placeholder="Номер телефона" name="phone" v-model="phone"
-              required /></div>
-              <span class="required-star required-star-phone">*</span>
-          </div>
-          <div class="input-wrapper">
-            <input class="required-input" type="email" placeholder="Email" name="email" v-model="email" required />
-            <span class="required-star required-star-email">*</span>
-          </div>
-          <div>
-            <input type="text" placeholder="Интересующий товар/услуга" name="subject" v-model="subject" required />
-          </div>
+  <h1>Связаться с нами</h1>
+  <div class="form-image-container">
+    <form @submit.prevent="submitForm">
 
-          <div class="input-wrapper">
-            <textarea class="required-input" name="message" rows="6" placeholder="Сообщение" v-model="message" required></textarea>
-            <span class="required-star required-star-message">*</span>
-          </div>
-
-          <p class="privacy-policy">Отправляя заявку Вы соглашаетесь с политикой конфиденциальности</p>
-          <button class="submit-button" type="submit">Отправить <i class="fas fa-arrow-right"></i></button>
-        </form>
+      <div class="input-container">
+        <input type="name" placeholder="Имя" name="name" v-model="name" required />
       </div>
-      <div class="image-container"><img class="man-image" src="./assets/onthephone.png"
-          alt="The man is talking on the phone"></div>
-    </div>
+
+
+
+      <input class="required-input" type="phone" placeholder="Номер телефона*" name="phone" v-model="phone" required />
+
+
+
+      <input class="required-input" type="email" placeholder="E-mail*" name="email" v-model="email" required/>
+
+
+      <input type="text" placeholder="Интересующий товар/услуга" name="subject" v-model="subject" required />
+
+
+
+      <textarea class="required-input" name="message" rows="6" placeholder="Сообщение*" v-model="message"
+        required></textarea>
+
+    </form>
+
+    <div class="image-container"><img src="./assets/onthephone.png" alt="The man is talking on the phone"></div>
   </div>
+  <div class="privacy-policy-container">
+    <p class="privacy-policy">Отправляя заявку Вы соглашаетесь с политикой конфиденциальности</p>
+  </div>
+  <button class="submit-button" type="submit">
+    ОТПРАВИТЬ
+    <div class="arrow-container"><img
+        src="/Users/vadimkim/Desktop/contact-form-assessment/contact-form-assessment/src/assets/arrow-2-right-long.png" />
+    </div>
+  </button>
+
 </template>
 
 <style scoped>
+.form-image-container {
+  display: flex;
+  flex-direction: row;
+  width: 1170px;
+  height: 369px;
+  gap: 30px;
+  padding-right: 0px;
+}
 
-.required-input-wrapper {
+.image-container {
+  width: 749px;
+  height: 369px;
+  overflow: hidden;
+}
+
+.privacy-policy-container {
+  width: 331px;
+  height: 34px;
+  margin-left: 40px;
+  margin-top: 22px;
+}
+
+.submit-button {
+  font-size: 12px;
+  font-weight: 400;
+  background-color: #333333;
+  width: 221.81px;
+  height: 71px;
+  color: white;
+  padding: 24px 47px;
+  border: none;
+  cursor: pointer;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+}
+
+.arrow-container {
+  width: 24px;
+  height: 24px;
+}
+
+
+/* .required-input-wrapper {
   position:relative
 }
 
@@ -79,28 +129,11 @@
   color: red;
 }
 
-.submit-button {
-  background-color: #333333;
-  width: 222px;
-  height: 71px;
-  color: white;
-  padding: 24px 47px;
-  font-size: 12px;
-  font-weight: 400;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  text-align: center;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 12px;
-}
 
 .submit-button:hover {
   background-color: #666666;
-}
+} */
 </style>
 
 <script>
