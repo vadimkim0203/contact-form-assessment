fe672cc44474192370e9f51fe7af0377e0fe7fef
Vadim Kim <vkim0203@gmail.com>
2024-12-21 18:41:50 +0900

    check logo added, asterisks colored in red

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,26 @@
       <div class="input-container">
         <input type="name" placeholder="Имя" name="name" v-model="name" required />
       </div>
-      <input class="required-input" type="phone" placeholder="Номер телефона*" name="phone" v-model="phone" required />
-      <input class="required-input" type="email" placeholder="E-mail*" name="email" v-model="email" required />
-      <input type="text" placeholder="Интересующий товар/услуга" name="subject" v-model="subject" required />
-      <textarea class="required-input" name="message" rows="6" placeholder="Сообщение*" v-model="message"
-        required></textarea>
+      <div class="input-container">
+        <input class="required-input" type="phone" placeholder="Номер телефона" name="phone" v-model="phone" required />
+        <span class="required-asterisk required-asterisk-phone">*</span>
+      </div>
+      <div class="input-container">
+        <input class="required-input" type="email" placeholder="E-mail" name="email" v-model="email" required />
+        <span class="required-asterisk required-asterisk-email">*</span>
+      </div>
+
+      <div class="input-container"><input type="text" placeholder="Интересующий товар/услуга" name="subject"
+          v-model="subject" required /></div>
+      <div class="input-container">
+        <textarea class="required-input" name="message" rows="4" placeholder="Сообщение" v-model="message"
+          required></textarea>
+        <span class="required-asterisk required-asterisk-message">*</span>
+        <div class="check-container"><img class="check-logo"
+            src="/Users/vadimkim/Desktop/contact-form-assessment/contact-form-assessment/src/assets/check.png" /></div>
+      </div>
+
+
     </form>
     <div class="image-container"><img src="./assets/onthephone.png" alt="The man is talking on the phone"></div>
   </div>
@@ -58,10 +73,11 @@
 .privacy-policy-container {
   font-weight: 400;
   font-size: 14px;
-
   width: 331px;
   height: 34px;
   margin-left: 40px;
+  margin-bottom: 21px;
+
 }
 
 .submit-button {
@@ -81,7 +97,7 @@
   justify-content: center;
   gap: 12px;
   position: absolute;
-  bottom: 0px;
+  bottom: -9px;
   left: 0px;
   letter-spacing: 0.2em;
 }
@@ -89,6 +105,38 @@
 .arrow-container {
   width: 24px;
   height: 24px;
+}
+
+.input-container {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+
+}
+
+.required-asterisk {
+  color: red;
+  font-size: 1rem;
+  /* Adjust size as needed */
+  position: absolute;
+}
+
+.required-asterisk-phone {
+  left: 138px;
+}
+
+.required-asterisk-email {
+  left: 64px;
+}
+
+.required-asterisk-message {
+  top: 12px;
+  left: 99px;
+}
+
+.check-container {
+  position: absolute;
+  left: 6px;
 }
 </style>
 
